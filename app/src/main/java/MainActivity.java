d82f826eb8597c6c81312345e40a237c25c3b212
codetribe <cumbudzor@gmail.com>
2018-02-06 10:10:12 +0200

    Initial commit

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,18 @@
+
+import android.os.Bundle;
+;
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.example.codetribe.thebook.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
